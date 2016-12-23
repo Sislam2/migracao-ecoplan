4621214139c30f5809f94dd0e01c932bd421274b
cotelha <rafael.cote@gmail.com>
2016-12-23 11:51:28 -0200

    add relationships

--- app/models/type.rb
+++ app/models/type.rb
@@ -3,6 +3,7 @@ class Type < ActiveRecord::Base
   self.primary_key = 'tipo'
 
   has_many :licenses, class_name: "License", foreign_key: "tipo"
+  belongs_to :type_document, class_name: "TypeDocument", foreign_key: "licensa"
 
   rails_admin do
     # ...
--- app/models/type_document.rb
+++ app/models/type_document.rb
@@ -2,6 +2,8 @@ class TypeDocument < ActiveRecord::Base
   self.table_name = "tiposprocessos"
   self.primary_key = 'id'
 
+  has_many :types, class_name: "Type", foreign_key: "licensa"
+
   rails_admin do
     # ...
   end
